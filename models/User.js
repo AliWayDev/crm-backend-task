699c6456f5e8d2b14e22f51fda21932bdb4d6a15
AliWayDev <alinurlibekov@gmail.com>
2024-09-20 15:17:56 +0500

    added uniqe param to model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import { Schema, model } from "mongoose";
 const User = new Schema({
   id: { type: String },
   name: { type: String },
-  email: { type: String },
+  email: { type: String, unique: true },
   lastActivity: { type: String },
   status: { type: String },
   password: { type: String }
